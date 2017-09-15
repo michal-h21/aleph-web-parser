f71f0e9363af92305c15556f7896ce733409c583
Michal Hoftich <michal.h21@gmail.com>
2017-09-15 15:54:11 +0200

    Added ccl support

--- getaleph.lua
+++ getaleph.lua
@@ -48,7 +48,7 @@ function Opac:get_www(url)
   return www:url(url):get_body()
 end
 
---- Get HTML content form an URL, cleaned using HTML Tidy and stripped of <script> tags comments.
+--- Get HTML content form an URL, cleaned using HTML Tidy and stripped of script tags comments.
 -- @param url
 -- @return HTML code
 function Opac:get_clean_www(url)
@@ -62,7 +62,7 @@ function Opac:get_clean_www(url)
 end
 
 --- Build new URL for Aleph using the session number
--- @param url to be fetched
+-- @param new to be fetched
 -- @return correct url
 function Opac:build_url(new)
   local new = new
@@ -103,7 +103,7 @@ end
 
 
 
---- 
+--- Search using URL
 -- @param query URL to be searched 
 -- @return table with search results URLs
 function Opac:search_query(query)
@@ -111,14 +111,47 @@ function Opac:search_query(query)
   local new = self:build_url(query)
   local body = self:get_clean_www(new)
   local result_table = self:get_result(body)
+  return result_table
 end
 
-function Opac:search(options)
+--- Build CCL query
+-- @param options Table with CCL fields
+-- It should look like: {{SYS=187872}, "or", {SYS=123322}}
+function Opac:build_ccl_query(options)
+  local query = {}
+  local logical 
+  for _, object in ipairs(options) do
+    -- string object can be and, or, not
+    if type(object) == "string" then
+      logical = object
+    elseif type(object) == "table" then
+      -- keys should be CCL code, value can be string or table with multiple values which will be joined using "or"
+      for k,v in pairs(object) do
+        local values
+        if type(v) == "table" then
+          values = table.concat(v, "+")
+        else
+          values = v
+        end
+        local current_query = string.format("(%s=%s)", k, values)
+        query[#query+1] = logical
+        query[#query+1] = current_query
+        logical = "and"
+      end
+    end
+
+  end
+  return table.concat(query, "+")
   -- https://ckis.cuni.cz/F/KA1TUX3X6RLRUDJ8CB4C7I5UQVAFSEHR8D2EMCCT9RR9YJU8ME-46290?func=find-c&ccl_term=(wau=carlyle+or+ruskin+or+hegel)+and+(wti=kultur?)&adjacent=N&local_base=CKS&x=42&y=11&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFM&filter_request_4=&filter_code_5=WSL&filter_request_5=
 
 end
 
-
+--- Search using CCL table query
+function Opac:search_ccl(query)
+  local ccl = self:build_ccl_query(query)
+  local search = string.format("?func=find-c&ccl_term=%s", ccl)
+  return self:search_query(search)
+end
 
 
 local opac = Opac.new("https://ckis.cuni.cz/F/")
@@ -133,6 +166,9 @@ print(opac.base_url)
 opac:search_query( "?func=find-c&ccl_term=SYS=1878726&local_base=CKS")
 
 opac:search_query("?func=find-c&ccl_term=(wau=carlyle+or+ruskin+or+hegel)")
+
+opac:search_ccl({{SYS=187872}, "or", {SYS=123322}})
+
 -- opac:search( "?func=find-c&ccl_term=SYS=1878726&local_base=CKS&x=0&y=0&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFM&filter_request_4=&filter_code_5=WSL&filter_request_5=")
 -- https://ckis.cuni.cz/F/AJG1KRKT2RSVJMN1QACVQ4XGH2XFLIYNV7ND836QU1UU362IAB-35717?func=find-c&ccl_term=sys=1878726&adjacent=N&local_base=CKS&x=0&y=0&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFM&filter_request_4=&filter_code_5=WSL&filter_request_5=
 -- https://ckis.cuni.cz:443/F/9VQ26UBEQBE7N7NY8FKVBD9THK99QQNS5XIXGVTBXUUGCHRTIH-37482?func=find-c&ccl_term=sys=1878726&adjacent=N&local_base=CKS&x=0&y=0&filter_code_1=WLN&filter_request_1=&filter_code_2=WYR&filter_request_2=&filter_code_3=WYR&filter_request_3=&filter_code_4=WFM&filter_request_4=&filter_code_5=WSL&filter_request_5=
