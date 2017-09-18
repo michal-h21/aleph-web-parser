c0f194c9d05a8e9e412dcb23158fd203718d035c
Michal Hoftich <michal.h21@gmail.com>
2017-09-18 15:54:47 +0200

    print konspekt

--- konspekt.lua
+++ konspekt.lua
@@ -4,6 +4,26 @@ local cjson = require "cjson"
 local opac = Opac.new("https://ckis.cuni.cz/F/")
 
 
+local function get_konspekt(marc)
+  local konspekty = {}
+  for k,v in pairs(marc["072"] or {}) do
+    print("xxxL",k,v)
+  end
+  local records = marc["072"] or {}
+  for _, fields in ipairs(records) do
+    for _,x in ipairs(fields.fields or {}) do
+      print(x.name, x.value)
+      if x.name == "a" then
+        table.insert(konspekty, x.value)
+      end
+    end
+  end
+  return table.concat(konspekty, ";")
+end
+
+
+
+
 
 print(opac.base_url)
 
@@ -21,17 +41,21 @@ for _,v in ipairs(results) do print(v); table.insert(all, cjson.encode(opac:get_
 
 
 local marc = opac:get_sysno(1175616)
-for k,v in pairs(marc) do
-  -- print(k, table.concat(v.indicators or {}, ":"), table.concat(
-  print(k, #v)
-end
+-- for k,v in pairs(marc) do
+-- print(k, table.concat(v.indicators or {}, ":"), table.concat(
+-- print(k, #v)
+-- end
 
 local json_data = cjson.encode(marc)
 
 table.insert(all, json_data)
 
 local full_json = "[".. table.concat(all, ",\n") .. "]"
-print(json_data)
+-- print(json_data)
 
-print(#cjson.decode(full_json))
+-- testujeme funkčnost ukládání do json
+local decoded = cjson.decode(full_json)
 
+for _,y in ipairs(decoded) do
+  print(get_konspekt(y))
+end
